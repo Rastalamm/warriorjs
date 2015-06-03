9092f048e1129a1144dff0ad94113f063ff9fe8a
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-02 19:45:13 -1000

    Got to level 5 of warrior game

--- warriorjs/rastalamm-beginner/Player.js
+++ warriorjs/rastalamm-beginner/Player.js
@@ -1,6 +1,22 @@
 class Player {
   playTurn(warrior) {
-    // Cool code goes here
+
+    this._health;
+
+
+    if(!warrior.feel().isEmpty()){
+      warrior.attack();
+    }else if(warrior.feel().isEmpty() && warrior.health() < this._health){
+      warrior.walk()
+    }else if(warrior.feel().isEmpty() && warrior.health() < 15){
+      warrior.rest();
+    }else{
+      warrior.walk()
+    }
+    this._health = warrior.health();
+
+
+
   }
 }
 
